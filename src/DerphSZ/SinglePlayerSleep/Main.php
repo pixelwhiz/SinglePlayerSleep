f611581a26bb3cfe862d1189f8771df402d15e89
DerphSZ <70942403+DerphSZ@users.noreply.github.com>
2021-04-17 11:07:50 +0700

    Fix Bugs on set the timer

--- src/DerphSZ/SinglePlayerSleep/Main.php
+++ src/DerphSZ/SinglePlayerSleep/Main.php
@@ -17,36 +17,42 @@ use pocketmine\event\Listener;
 
 class Main extends PluginBase implements Listener {
 	
-	public const TIME_DAY = 1000;
-	public const TIME_NOON = 6000;
-	public const TIME_SUNSET = 12000;
-	public const TIME_NIGHT = 13000;
-	public const TIME_MIDNIGHT = 18000;
-	public const TIME_SUNRISE = 23000;
+    public const TIME_DAY = 1000;
+    public const TIME_NOON = 6000;
+    public const TIME_SUNSET = 12000;
+    public const TIME_NIGHT = 13000;
+    public const TIME_MIDNIGHT = 18000;
+    public const TIME_SUNRISE = 23000;
 
-	public const TIME_FULL = 24000;
+    public const TIME_FULL = 24000;
 	
-	public function onEnable(){
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-	}
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
 	
-	public function onEnterBed(PlayerBedEnterEvent $event) {
+    public function onEnterBed(PlayerBedEnterEvent $event) {
         $player = $event->getPlayer();
         $this->getScheduler()->scheduleDelayedTask(new SleepTask ($this), 20 * 3);
     }
 	
-	public function onDisable(){
-	}
+    public function onDisable(){
+    }
+
 }
 
 class SleepTask extends Task {
-	public function __construct(Main $main){
-		$this->main = $main;
-	}
-	public function onRun($tick){
-		foreach($this->main->getServer()->getOnlinePlayers() as $player){
-			$player->getLevel()->setTime(Main::TIME_SUNRISE);
-			$player->stopSleep();
-		}
-	}
+    public function __construct(Main $main){
+        $this->main = $main;
+    }
+    public function onRun($tick){
+        foreach($this->main->getServer()->getOnlinePlayers() as $player){
+            if($player->isSleeping()){
+                $player->getLevel()->setTime(Main::TIME_SUNRISE);
+                $player->stopSleep();
+	    }
+            return true;
+        }
+		
+    }
 }	
+	
