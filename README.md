3383e3bb77dc88b8011f33c45a18e542aa2f9a4a
M. Daffa Teuku F.A <111522987+pixelwhiz@users.noreply.github.com>
2023-11-18 11:09:53 +0700

    Update to PM 5.0.0

---
+++ README.md
@@ -0,0 +1,5 @@
+![minecraft-stuck-in-bed](https://user-images.githubusercontent.com/70942403/111478134-5ce52d00-8762-11eb-9ba0-56b665fb09fd.jpg)
+# Description
+This plugin is to make it easier and faster for player to sleep when you play multiplayer servers (pocketmine).
+# Download
+> You can download this plugin at [Here](https://poggit.pmmp.io/ci/DerphSZ/SinglePlayerSleep)
---
+++ src/Main.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pixelwhiz\singleplayersleep;
+
+use pocketmine\event\player\PlayerBedEnterEvent;
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\scheduler\Task;
+
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener {
+
+    public const TIME_DAY = 1000;
+    public const TIME_NOON = 6000;
+    public const TIME_SUNSET = 12000;
+    public const TIME_NIGHT = 13000;
+    public const TIME_MIDNIGHT = 18000;
+    public const TIME_SUNRISE = 23000;
+
+    public const TIME_FULL = 24000;
+
+    public function onEnable() : void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onEnterBed(PlayerBedEnterEvent $event) {
+        $player = $event->getPlayer();
+        $this->getScheduler()->scheduleDelayedTask(new SleepTask ($this, $player->getName()), 20 * 5);
+    }
+
+}
+
+class SleepTask extends Task {
+    public function __construct(Main $main, $player){
+        $this->main = $main;
+        $this->player = $player;
+    }
+
+    public function onRun(): void
+    {
+        $player = $this->main->getServer()->getPlayerExact($this->player);
+        if($player->isSleeping()){
+            $player->getWorld()->setTime(Main::TIME_SUNRISE);
+            $player->stopSleep();
+        }
+    }
+
+}
